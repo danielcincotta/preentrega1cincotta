0d437adba31b35d1a68d69eaf993fcfb71fdb305
danielcincotta <daniel.cincotta@hotmail.com>
2023-10-23 11:09:52 -0300

    version5

--- ecommerce-5tta/src/componets/CartContainer/CartContainer.jsx
+++ ecommerce-5tta/src/componets/CartContainer/CartContainer.jsx
@@ -1,18 +1,19 @@
 import { useCartContext } from "../../context/CartContext"
 import { addDoc, collection, doc, getDoc, getDocs, getFirestore, limit, orderBy, query, updateDoc, where } from 'firebase/firestore'
-
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 
 function CartContainer() {
+    const [orderId, setOrderId] = useState("")
     const {deleteCart, deleteProd, totalPrice,  cartList} = useCartContext()
-
-const addOrder =  () =>{
-
+    
+    const addOrder = (name,email) =>{
     const order = {}
-    order.buyer = {name: "daniel", 
-                    email: "daniel@hotmail.com"
+    order.buyer = {name: name, 
+                    email: email
                 }
-    order.items = cartList.map(prod =>{
+    order.items = cartList.map(prod => {
         return {id: prod.id, name: prod.name , price: prod.price, quantity: prod.quantity}
         })
     order.total = totalPrice()
@@ -21,15 +22,19 @@ const addOrder =  () =>{
     const dataBase = getFirestore()
     const ordersCollection = collection(dataBase, "orders")
     addDoc(ordersCollection, order)
-    .then(
-        resp => console.log(resp)  
-    )
+    .then(({id}) => setOrderId(id))
+    //.then(
+      //  resp => ( { id: resp.id, ...resp.data() } )
+     //)
+    .then(resp =>{
+        console.log("order created", resp);
+    })
     
-    /*
+    
     order.items.forEach(element => {
         
         const db = getFirestore()
-        const queryDoc = doc(db, 'products', element.id) // id useParams
+        const queryDoc = doc(db, 'products', element.id) 
         getDoc(queryDoc)
         .then(
             resp => ( { id: resp.id, ...resp.data() } ) 
@@ -39,20 +44,19 @@ const addOrder =  () =>{
             const stockUpdate = parseInt(resp.stock) - element.quantity
                 const updateProduct = doc(dataBase, "products", element.id)
                 updateDoc( updateProduct , {stock: stockUpdate})
-            
+                deleteCart()
+
             } 
-        )
-            
-    })*/
-            
-    
-
-
+        )           
+    })
 }
 
 
   return (
-    <>           
+    <>       
+       {orderId != '' && <p>orden generada : {orderId}</p>}
+        {cartList.length > 0 ?
+        <div>
         {
             cartList.map(prod => 
                 <div key={prod.id}>
@@ -65,7 +69,19 @@ const addOrder =  () =>{
         <button onClick={ () => deleteCart() }>Vaciar Carrito</button>
         <h3>Precio total: {totalPrice()}</h3>
 
-        <form >
+        <form onSubmit={ev => {
+            ev.preventDefault()
+
+            const name = ev.target.name.value
+            const email = ev.target.email.value
+            if(name === "" && email === ""){
+              alert("debe completar el formulario")
+              return
+            }
+            addOrder(name,email)
+        }
+        
+        }>
                 <input 
                 id= "nombre"
                     type="text" 
@@ -79,14 +95,23 @@ const addOrder =  () =>{
                     type="text" 
                     name='email'
                     placeholder="ingrese el email"
-            
-                   
                 />
                 
-                <button onClick={addOrder}>Enviar</button>
+                <button type ="submit" >Enviar</button>
             </form>
+            </div>
+    : <center>
+    <h2>No hay productos en el carrito</h2>
+    <Link to='/'>ir a compras</Link>
+</center>
+    }
+
+
     </>
   )
+        
+
+
 }
 
 export default CartContainer
